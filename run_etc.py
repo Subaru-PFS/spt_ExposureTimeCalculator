51498303e034604f1d9ffa766a7998af4944e724
Kiyoto Yabe <kiyoto.yabe@ipmu.jp>
2016-02-23 11:29:11 +0900

    PFS-v5.1 updated (some information hidden) README & run_etc.py updated

--- run_etc.py
+++ run_etc.py
@@ -10,8 +10,8 @@ if __name__ == '__main__':
     ### CHANGE BELOW ON YOUR OWN RESPONSIBILITY ###
     ###############################################
     ETC            = 'gsetc_r104'
-    INSTR_SETUP    = './config/PFS-v5.0.dat'
-    INSTR_SETUP_MR = './config/PFS-v5.0.redMR.dat'
+    INSTR_SETUP    = './config/PFS-v5.1.dat'
+    INSTR_SETUP_MR = './config/PFS-v5.1.redMR.dat'
     SKYMODELS      = '11005'
     OFFSET_FIB     = 0.00
     SKY_SUB_FLOOR  = 0.01
