149ee7ef7acb0a374fba26f0fe53b3f9c7fcb806
Kiyoto Yabe <kiyoto.yabe@ipmu.jp>
2016-03-10 19:39:34 +0900

    gen_sim_spec.py updated files on ./out/ --> ./example/output/

--- gen_sim_spec.py
+++ gen_sim_spec.py
@@ -88,7 +88,7 @@ if __name__ == '__main__':
             wav.append(float(a[2]))
             mag.append(float(a[6]))
             snc.append(float(a[3]))
-            if param_value['MR_MODE'].lower() == 'yes':
+            if param_value['MR_MODE'].lower() == 'yes' or param_value['MR_MODE'].lower() == 'y':
                 if int(a[0]) == 0:
                     dsp.append(float(a[2])/R0)
                 elif int(a[0]) == 1:
@@ -112,6 +112,11 @@ if __name__ == '__main__':
 #  5  SKY         [10^-17 erg/s/cm^2/A]
 #  6  ARM         [0=blue,1=red,2=NIR,3=redMR]
 ''')
+    flx_o = []
+    err_o = []
+    var_o = []
+    msk_o = []
+    sky_o = []
     for i in range(len(wav)):
         fnu   = 10**(-0.4*(mag[i]+48.6))
         flam  = 3.0e18*fnu/(10*wav[i])**2/1e-17
@@ -120,83 +125,116 @@ if __name__ == '__main__':
         var = sigma**2
         msk = 0.0
         sky = 0.0
-        xps = 0.0
-        cfg = 0.0
+        flx_o.append(flx)
+        err_o.append(sigma)
+        var_o.append(var)
+        msk_o.append(msk)
+        sky_o.append(sky)
         file.write('%8.3f %12.4e %12.4e %2d %12.4e %1d\n' % (wav[i],flx,sigma,msk,sky,arm[i]))
     file.close()
     print "ASCII table was generated"
 ## FITS mode ##
+    if param_value['MR_MODE'].lower() == 'yes' or param_value['MR_MODE'].lower() == 'y':
+        resampling = 0.04 # resampled pix scale [nm/pix]
+    else:
+        resampling = 0.08 # resampled pix scale [nm/pix]
     if PYFITS_FLG == 1 and NUMPY_FLG == 1:
+## check wavelength boundary of arms ##
+        arm_wav_min = []
+        arm_wav_max = []
         for arm_num in arm_list:
-            tract = int(param_value['OUTFILE_TRACT'])
-            patch = param_value['OUTFILE_PATCH']
-            catid = int(param_value['OUTFILE_CATID'])
-            objid = param_value['OUTFILE_OBJID']
-            nvisit = int(param_value['OUTFILE_NVISIT'])
-            config = param_value['OUTFILE_CONFIG']
-            output_file = './out/pfsObject-%04d-%3s-%03d-%8s-%02d-0x%8s_arm%1d.fits' % (tract,patch,catid,objid,nvisit,config,arm_num)
-            config_file_name = 'pfsConfig-0x%8s.fits' % (config)
-            config_file = './out/%s' % (config_file_name)
             wav_a = []
-            dsp_a = []
-            flx = []
-            err = []
-            var  = []
             for i in range(len(wav)):
                 if arm[i] == arm_num:
-                    fnu   = 10**(-0.4*(mag[i]+48.6))
-                    flam  = 3.0e18*fnu/(10*wav[i])**2/1e-17
-                    sigma = flam/(snc[i]+offset)
                     wav_a.append(wav[i])
-                    dsp_a.append(dsp[i])
-                    flx.append(flam+rn.gauss(0.0,sigma))
-                    err.append(sigma)
-                    var.append(sigma**2)
-            Npix = len(wav_a)
-            msk = np.zeros(Npix)
-            sky = np.zeros(Npix)
-            xps = np.zeros(Npix)
-            cfg = 'TBD'
-            col1 = np.array(flx)
-            col2w = pf.Column(name = 'WAVELENGTH', format = 'E', array = np.array(wav))
-            col2f = pf.Column(name = 'FLUX', format = 'E', array = np.array(flx))
-            col2e = pf.Column(name = 'ERROR', format = 'E', array = np.array(err))
-            col2m = pf.Column(name = 'MASK', format = 'J', array = msk)
-            col3 = np.array([np.zeros(Npix),np.zeros(Npix),np.array(var),np.zeros(Npix),np.zeros(Npix)])
-            col4 = np.zeros((10,10))
-            col5 = np.array(msk)
-            col6 = np.array(sky)
-            col71 = pf.Column(name = 'pfsConfigId', format = '10A', array = np.array(['0x%s' % (config)]))
-            col72 = pf.Column(name = 'visit', format = 'J', array = np.array([nvisit]))
-            hdu1 = pf.ImageHDU(data = col1, header=None)
-            hdu2 = pf.BinTableHDU.from_columns([col2w,col2f,col2e,col2m])
-            hdu3 = pf.ImageHDU(data = col3, header=None)
-            hdu4 = pf.ImageHDU(data = col4, header=None)
-            hdu5 = pf.ImageHDU(data = col5, header=None)
-            hdu6 = pf.ImageHDU(data = col6, header=None)
-            hdu7 = pf.BinTableHDU.from_columns([col71,col72])
-            hdu = pf.PrimaryHDU()
-            hdulist = pf.HDUList([hdu])
-            hdulist.append(hdu1)
-            hdulist.append(hdu2)
-            hdulist.append(hdu3)
-            hdulist.append(hdu4)
-            hdulist.append(hdu5)
-            hdulist.append(hdu6)
-            hdulist.append(hdu7)
-            hdulist[1].header['EXTNAME'] = 'FLUX'
-            hdulist[2].header['EXTNAME'] = 'FLUXTBL'
-            hdulist[3].header['EXTNAME'] = 'COVAR'
-            hdulist[4].header['EXTNAME'] = 'COVAR2'
-            hdulist[5].header['EXTNAME'] = 'MASK'
-            hdulist[6].header['EXTNAME'] = 'SKY'
-            hdulist[7].header['EXTNAME'] = 'CONFIG'
-            for i in range(6):
-                hdulist[i+1].header['CRPIX'] = 1
-                hdulist[i+1].header['CRVAL'] = min(wav_a)
-                hdulist[i+1].header['CDELT'] = (max(wav_a)-min(wav_a))/(len(wav_a)*1.0)
-            os.system('rm -f %s' % (output_file))
-            hdulist.writeto(output_file)
+            arm_wav_min.append(min(wav_a))
+            arm_wav_max.append(max(wav_a))
+## here assumed that arm = 0, 1, 2 (1=MR if MR_MODE == 'yes') ##
+        if param_value['MR_MODE'].lower() == 'yes' or param_value['MR_MODE'].lower() == 'y':
+            arm_wav_rng_min = [arm_wav_min[0],arm_wav_min[1],arm_wav_min[2]]
+            arm_wav_rng_max = [arm_wav_max[0],arm_wav_max[1],arm_wav_max[2]]
+        else:
+            arm_wav_rng_min = [arm_wav_min[0],0.5*(arm_wav_min[1]+arm_wav_max[0]),0.5*(arm_wav_min[2]+arm_wav_max[1])]
+            arm_wav_rng_max = [0.5*(arm_wav_min[1]+arm_wav_max[0]),0.5*(arm_wav_min[2]+arm_wav_max[1]),arm_wav_max[2]]
+## Data resampling ##
+        wav_a = []
+        flx_a = []
+        err_a = []
+        var_a = []
+        msk_a = []
+        sky_a = []
+        for i in range(len(wav)):
+            for arm_num in arm_list:
+                if arm[i] == arm_num:
+                    if wav[i]>=arm_wav_rng_min[arm_num] and wav[i]<=arm_wav_rng_max[arm_num]:
+                        wav_a.append(wav[i])
+                        flx_a.append(flx_o[i])
+                        err_a.append(err_o[i])
+                        var_a.append(var_o[i])
+                        msk_a.append(msk_o[i])
+                        sky_a.append(sky_o[i])
+        Npix = len(wav_a)
+        sampling = (max(wav_a)-min(wav_a))/(Npix*1.0)
+        wav_n = min(wav_a) + resampling*np.arange(int(round((max(wav_a)-min(wav_a))/resampling)))
+        Npix_resampled = len(wav_n)
+#        print Npix,min(wav_a),max(wav_a),sampling,len(wav_n),min(wav_n),max(wav_n),resampling
+        flx_resampled = np.interp(wav_n,wav_a,flx_a)
+        err_resampled = np.interp(wav_n,wav_a,err_a)
+        var_resampled = np.interp(wav_n,wav_a,var_a)
+        msk_resampled = np.interp(wav_n,wav_a,msk_a)
+        sky_resampled = np.interp(wav_n,wav_a,sky_a)
+## pfsObject ##
+        tract = int(param_value['OUTFILE_TRACT'])
+        patch = param_value['OUTFILE_PATCH']
+        catid = int(param_value['OUTFILE_CATID'])
+        objid = param_value['OUTFILE_OBJID']
+        nvisit = int(param_value['OUTFILE_NVISIT'])
+        config = param_value['OUTFILE_CONFIG']
+        output_file = './out/pfsObject-%04d-%3s-%03d-%8s-%02d-0x%8s.fits' % (tract,patch,catid,objid,nvisit,config)
+        config_file_name = 'pfsConfig-0x%8s.fits' % (config)
+        config_file = './out/%s' % (config_file_name)
+        col1 = np.array(flx_resampled)
+        col2w = pf.Column(name = 'WAVELENGTH', format = 'E', array = np.array(wav_a))
+        col2f = pf.Column(name = 'FLUX', format = 'E', array = np.array(flx_a))
+        col2e = pf.Column(name = 'ERROR', format = 'E', array = np.array(err_a))
+        col2m = pf.Column(name = 'MASK', format = 'J', array = np.array(msk_a))
+        col3 = np.array([np.zeros(Npix_resampled),np.zeros(Npix_resampled),np.array(var_resampled),np.zeros(Npix_resampled),np.zeros(Npix_resampled)])
+        col4 = np.zeros((10,10))
+        col5 = np.array(msk_resampled)
+        col6 = np.array(sky_resampled)
+        col71 = pf.Column(name = 'pfsConfigId', format = '10A', array = np.array(['0x%s' % (config)]))
+        col72 = pf.Column(name = 'visit', format = 'J', array = np.array([nvisit]))
+        hdu1 = pf.ImageHDU(data = col1, header=None)
+        hdu2 = pf.BinTableHDU.from_columns([col2w,col2f,col2e,col2m])
+        hdu3 = pf.ImageHDU(data = col3, header=None)
+        hdu4 = pf.ImageHDU(data = col4, header=None)
+        hdu5 = pf.ImageHDU(data = col5, header=None)
+        hdu6 = pf.ImageHDU(data = col6, header=None)
+        hdu7 = pf.BinTableHDU.from_columns([col71,col72])
+        hdu = pf.PrimaryHDU()
+        hdulist = pf.HDUList([hdu])
+        hdulist.append(hdu1)
+        hdulist.append(hdu2)
+        hdulist.append(hdu3)
+        hdulist.append(hdu4)
+        hdulist.append(hdu5)
+        hdulist.append(hdu6)
+        hdulist.append(hdu7)
+        hdulist[1].header['EXTNAME'] = 'FLUX'
+        hdulist[2].header['EXTNAME'] = 'FLUXTBL'
+        hdulist[3].header['EXTNAME'] = 'COVAR'
+        hdulist[4].header['EXTNAME'] = 'COVAR2'
+        hdulist[5].header['EXTNAME'] = 'MASK'
+        hdulist[6].header['EXTNAME'] = 'SKY'
+        hdulist[7].header['EXTNAME'] = 'CONFIG'
+        for i in [0,2,3,4,5]:
+            hdulist[i+1].header['CTYPE1'] = 'WAVELENGTH'
+            hdulist[i+1].header['CUNIT1'] = 'nm'
+            hdulist[i+1].header['CRPIX1'] = 1.0
+            hdulist[i+1].header['CRVAL1'] = min(wav_n)
+            hdulist[i+1].header['CD1_1'] = resampling
+        os.system('rm -f %s' % (output_file))
+        hdulist.writeto(output_file)
 ## pfsConfig ##
 # catId, objId, ra, dec, fiber flux, MPS centroid
         col1 = pf.Column(name = 'catId', format = 'J', array = np.array([catid]))
